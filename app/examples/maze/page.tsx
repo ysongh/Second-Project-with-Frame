33c24dc0ed22b3a98e72c4daa94934432d97533b
You Song Hou <ysongweb3@gmail.com>
2024-02-19 01:38:54 -0500

    feat: implement movement functionality for the buttons to move the image

--- app/examples/maze/page.tsx
+++ app/examples/maze/page.tsx
@@ -12,14 +12,25 @@ import Link from "next/link";
 import { DEBUG_HUB_OPTIONS } from "../../debug/constants";
 
 type State = {
-  count: number;
+  x: number;
+  y: number;
 };
 
-const initialState: State = { count: 0 };
+const initialState: State = { x: 0, y: 0 };
 
 const reducer: FrameReducer<State> = (state, action) => {
+  const buttonIndex = action.postBody?.untrustedData.buttonIndex;
   return {
-    count: 1
+    x: buttonIndex === 3
+      ? state.x - 1
+      : buttonIndex === 4
+        ? state.x + 1
+        : state.x,
+    y: buttonIndex === 1
+      ? state.y - 1
+      : buttonIndex === 2
+        ? state.y + 1
+        : state.y
   };
 };
 
@@ -69,14 +80,16 @@ export default async function Home({
               src="https://images.unsplash.com/photo-1574390353491-92705370c72e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWF6ZXxlbnwwfDB8MHx8fDI%3D"
               alt="Water"
               style={{
-                width: "50%"
+                width: "50px",
+                marginTop: state.y * 20,
+                marginLeft: state.x * 20
               }} />
             <div style={{
               display: "flex",
               flexDirection: "column",
             }}>
-              <p>Count is {state.count}</p>
-              <p>{frameMessage?.inputText}</p>
+              <p>X is {state.x}</p>
+              <p>Y is {state.y}</p>
             </div>
           </div>
         </FrameImage>
